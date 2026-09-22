6df148aa4cdfb3cda5c5264b1ef43571f937486e
buenoIago <iagocedup@gmail.com>
2026-09-22 17:47:06 -0300

    excluindo chave de jwt do setup user-secret

--- scripts/setup-user-secrets.ps1
+++ scripts/setup-user-secrets.ps1
@@ -14,19 +14,11 @@ $bytes = New-Object byte[] 48
 [System.Security.Cryptography.RandomNumberGenerator]::Fill($bytes)
 $chaveJwt = [Convert]::ToBase64String($bytes)
 
-dotnet user-secrets set "Jwt:Key" "$OXIESvZA6fGd7way8Vz5B4FpbchPTrCL" --project "src/Api"
+dotnet user-secrets set "Jwt:Key" "" --project "src/Api"
 
 dotnet user-secrets set "ConnectionStrings:PostgresEF" `
   "Host=localhost;Port=5432;Database=GeradorCertificadoAppDb;Username=postgres;Password=postgres" `
-  --project dotnet user-secrets set "Jwt:Key" "$OXIESvZA6fGd7way8Vz5B4FpbchPTrCL" --project "src/Api"
-
-dotnet user-secrets set "ConnectionStrings:PostgresEF" `
-  "Host=localhost;Port=5432;Database=GeradorCertificadoAppDb;Username=postgres;Password=postgres" `
-  --project "src/Api"
-
-dotnet user-secrets set "ConnectionStrings:RabbitMq" `
-  "amqp://guest:guest@localhost:5672" `
-  --project "src/Api"
+  --project 
 
 dotnet user-secrets set "ConnectionStrings:RabbitMq" `
   "amqp://guest:guest@localhost:5672" `
