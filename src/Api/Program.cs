2b0ecd5746be5add837f2d5f8c1708f45c5bad10
buenoIago <iagocedup@gmail.com>
2026-09-23 12:13:35 -0300

    ajustando swagger

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -284,12 +284,12 @@ catch (Exception ex)
 // SWAGGER
 // ============================================================
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() ||
+    app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 // ============================================================
 // BANCO DE DADOS
 // ============================================================
